e737412379fda3613612a08c1d70ff1fcbccb6b5
Hiren Vasoya <hiren.vasoya@artoon.in>
2023-07-01 15:38:07 +0530

    add code

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# python_crud
+## Model Based Validator design for Flask (Python) and Mongo DB
+#### Model based custom validation for flask (python). Use Flask server and mongo db. Integrate with any SPA API development.  
+
+### Create [virtual environment]('https://docs.python.org/3/library/venv.html) and install requirements 
+```sh
+pip install -r requirements.txt
+```
+### Configure Database
+#### From [db_config.json](src/db_config.json) configure datbase url, name, user and password 
+```json
+ {
+   "db": {
+            "url" : "mongodb://localhost:27017/",
+            "name" :"db_name",  
+            "user" :"",
+            "password" :""
+    }
+ }
+``` 
+
+## In model update collection name and desire fields name and fields type. For example see users [model](src/models/users.py) file
+#### From [model](src/models) folder write your individual model and configure db collection name, fields name and fields type
+#### Example
+##### In users [model](src/models/users.py) update collection name, fields name and fields type
+```py
+table_name = 'users'   # collection name
+fields = {   
+    "name": "string",
+    "email":"string",
+    "mobile": "number",
+    "password": "string",
+    "dob": "datetime",
+    "profile": "string"
+} 
+```
+#### List of users Routes
+| Request | Endpoint |  Details |
+| --- | --- | --- |
+| `GET` | `http://127.0.0.1:5000/users`| Get All users|
+| `GET` | `http://127.0.0.1:5000/users/user_id`| Get Single user|
+| `POST` | `http://127.0.0.1:5000/users/cretae`| Insert One user|
+| `PUT` | `http://127.0.0.1:5000/users/update/user_id`| Update One user|
+| `DELETE` | `http://127.0.0.1:5000/users/update/user_id`| Delete One user|
+| `POST` | `http://127.0.0.1:5000/upload/user_id`| update profile|
+| `DELETE` | `http://127.0.0.1:5000/users/login`| login|
+- To see route list type cli `flask routes`
+
+### Lets run the App
+```sh
+python app.py 
+```
---
+++ src/models/users.py
@@ -0,0 +1,58 @@
+from helper.validation import Validator
+from helper.database import Database
+
+
+class Users(object):
+    def __init__(self):
+        self.validator = Validator()
+        self.db = Database()
+
+        self.collection_name = 'users'  # collection name
+
+        self.fields = {
+            "name": "string",
+            "email": "string",
+            "mobile": "int",
+            "password": "string",
+            "dob": "datetime",
+            "profile": "string",
+            "createdAt": "datetime",
+            "updatedAt": "datetime",
+        }
+
+        self.create_required_fields = ["name", "mobile", "email", "password"]
+
+        # Fields optional for CREATE
+        self.create_optional_fields = []
+
+        # Fields required for UPDATE
+        self.update_required_fields = ["name", "mobile"]
+
+        # Fields optional for UPDATE
+        self.update_optional_fields = []
+
+    def create(self, users):
+        # Validator will throw error if invalid
+        self.validator.validate(users, self.fields, self.create_required_fields, self.create_optional_fields)
+        res = self.db.insert(users, self.collection_name)
+        return "Inserted Id " + res
+
+    def find(self, users):  # find all
+        return self.db.find(users, self.collection_name)
+
+    def find_one(self, users):  # find One
+        print('-----',users)
+        return self.db.find_one(users, self.collection_name)
+
+    def find_by_id(self, id):
+        return self.db.find_by_id(id, self.collection_name)
+
+    def update(self, id, users):
+        self.validator.validate(users, self.fields, self.update_required_fields, self.update_optional_fields)
+        return self.db.update(id, users,self.collection_name)
+
+    def updateOne(self, id, users):
+        return self.db.update(id, users,self.collection_name)
+
+    def delete(self, id):
+        return self.db.delete(id, self.collection_name)
